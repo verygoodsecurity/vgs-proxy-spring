2b0001dbb7b4a1fe0653f6f0a81ae5b1d098cdd9
Oleg Yatskiv <olegyatskiv@lpt-C02TH0RZG8WN.corp.verygood.systems>
2018-07-12 17:24:02 +0300

    Removed unused exception

--- lib/src/main/java/com/verygood/security/integration/proxy/exception/VgsForwardProxyUrlFormatException.java
+++
@@ -1,9 +0,0 @@
-package com.verygood.security.integration.proxy.exception;
-
-public class VgsForwardProxyUrlFormatException extends RuntimeException {
-
-  public VgsForwardProxyUrlFormatException(String message){
-    super(message);
-  }
-
-}
