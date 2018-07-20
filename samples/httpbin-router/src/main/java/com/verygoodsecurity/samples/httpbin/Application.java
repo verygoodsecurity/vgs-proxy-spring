221b2d934ef5cb846ef1ed214b82093640fa96f3
Yuriy Yunikov <yyunikov@gmail.com>
2018-07-20 14:40:06 +0300

    Cleaning up sample application (#5)
    * Cleaning up sample application
    
    * Fixing README
    
    * Fixing README
    
    * Fixing README
    
    * Fixed code review comments

--- samples/httpbin-router/src/main/java/com/verygoodsecurity/samples/httpbin/Application.java
+++ samples/httpbin-router/src/main/java/com/verygoodsecurity/samples/httpbin/Application.java
@@ -1,13 +1,23 @@
-package com.verygood.security.demo;
+package com.verygoodsecurity.samples.httpbin;
 
 
 import com.verygoodsecurity.spring.annotation.EnableVgsProxy;
+import com.verygoodsecurity.spring.annotation.VgsProxied;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableVgsProxy
 public class Application {
+
+  @Bean
+  @VgsProxied
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
