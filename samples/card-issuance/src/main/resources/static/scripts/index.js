61ab260d64d860bd8f03f167840fe50d8ea3007d
Bohdan Khablenko <bkhablenko@gmail.com>
2018-08-09 14:57:10 +0300

    Add README with instructions on how to run application

--- samples/card-issuance/src/main/resources/static/scripts/index.js
+++ samples/card-issuance/src/main/resources/static/scripts/index.js
@@ -1,54 +1,54 @@
 $(document).ready(function () {
-  let togglePanels = function () {
-    let array = ['#apply-panel', '#success-panel'];
-    $.each(array, function (index, element) {
-      $(element).toggle();
-    });
-  };
-
-  let serializeJSON = function (form) {
-    let object = {};
-
-    let array = $(form).serializeArray();
-    $.each(array, function () {
-      object[this.name] = this.value;
-    });
-
-    return JSON.stringify(object);
-  };
-
-  $('#user-form').submit(function (event) {
-    event.preventDefault();
-
-    let json = serializeJSON(this);
-    console.log(json);
-
-    $('#spinner').show();
-    $.ajax({
-      type: 'POST',
-      url: 'https://tntlm8fhgcf.SANDBOX.verygoodproxy.io/cards',
-      contentType: 'application/json',
-      data: json,
-      success: function (response) {
-        console.log(response);
-        $.each(response, function (name, value) {
-          let element = $('#' + name);
-          if (element) {
-            element.text(value || '');
-          }
+    let togglePanels = function () {
+        let array = ['#apply-panel', '#success-panel'];
+        $.each(array, function (index, element) {
+            $(element).toggle();
         });
-        togglePanels();
-      },
-      error: function (jqXHR) {
-        alert(jqXHR.responseText);
-      },
-      complete: function () {
-        $('#spinner').hide();
-      }
-    });
-  });
+    };
 
-  $('#apply-again-btn').click(function () {
-    togglePanels();
-  });
+    let serializeJSON = function (form) {
+        let object = {};
+
+        let array = $(form).serializeArray();
+        $.each(array, function () {
+            object[this.name] = this.value;
+        });
+
+        return JSON.stringify(object);
+    };
+
+    $('#user-form').submit(function (event) {
+        event.preventDefault();
+
+        let json = serializeJSON(this);
+        console.log(json);
+
+        $('#spinner').show();
+        $.ajax({
+            type: 'POST',
+            url: 'INBOUND_ROUTE_URL/cards', // See README.md
+            contentType: 'application/json',
+            data: json,
+            success: function (response) {
+                console.log(response);
+                $.each(response, function (name, value) {
+                    let element = $('#' + name);
+                    if (element) {
+                        element.text(value || '');
+                    }
+                });
+                togglePanels();
+            },
+            error: function (jqXHR) {
+                alert(jqXHR.responseText);
+            },
+            complete: function () {
+                $('#spinner').hide();
+            }
+        });
+    });
+
+    $('#apply-again-btn').click(function () {
+        togglePanels();
+    });
 });
