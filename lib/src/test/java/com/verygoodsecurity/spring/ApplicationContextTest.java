6cec55c50f6c996997e7a543081cb77c74fd4ecb
Oleh Yatskiv <oleh.yatskiv@vgs.io>
2018-08-28 12:21:10 +0300

    Added basic tests (#14)
    * Added basic tests
    
    * Cleanup

---
+++ lib/src/test/java/com/verygoodsecurity/spring/ApplicationContextTest.java
@@ -0,0 +1,31 @@
+package com.verygoodsecurity.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {VgsProxyConfiguration.class, RestTemplateConfig.class}, loader= AnnotationConfigContextLoader.class)
+@TestPropertySource("classpath:application.properties")
+public class ApplicationContextTest extends AbstractJUnit4SpringContextTests {
+
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  @Value("${vgs.proxy.url}")
+  private String forwardProxyUrl;
+
+  @Test
+  public void testAppContext() {
+    applicationContext.getBean(RestTemplate.class);
+  }
+
+}
---
+++ lib/src/test/java/com/verygoodsecurity/spring/VgsProxyConfigurationTest.java
@@ -0,0 +1,57 @@
+package com.verygoodsecurity.spring;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Collections;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VgsProxyConfigurationTest {
+
+  private static final String TRUST_STORE_RESOURCE_NAME = "certs/vgs-proxy.jks";
+  private static final String TRUST_STORE_PASSWORD = "verygoodproxy";
+  private static final String forwardProxyUrl = "https://username:password@subdomain.SANDBOX.verygoodproxy.com:8080";
+
+  private RestTemplate restTemplate;
+
+  private VgsProxyConfiguration vgsProxyConfiguration;
+
+  @Before
+  public void setUp() {
+    restTemplate = Mockito.mock(RestTemplate.class);
+    vgsProxyConfiguration = new VgsProxyConfiguration(forwardProxyUrl);
+    vgsProxyConfiguration.setRestTemplates(Collections.singletonList(restTemplate));
+  }
+
+  @Test
+  public void testConfigureRestTemplates() {
+    // When
+    vgsProxyConfiguration.configureRestTemplates();
+
+    // Then
+    verify(restTemplate).setRequestFactory(any());
+  }
+
+  @Test
+  public void testKeyStoreLoads() throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException {
+    final ClassLoader classLoader = getClass().getClassLoader();
+    final KeyStore vgsProxyCerts = KeyStore.getInstance(KeyStore.getDefaultType());
+    try (InputStream inputStream = classLoader.getResourceAsStream(TRUST_STORE_RESOURCE_NAME)) {
+      vgsProxyCerts.load(inputStream, TRUST_STORE_PASSWORD.toCharArray());
+    }
+  }
+
+}
